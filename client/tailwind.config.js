139bc6cfa0a060b26a082468580ce17bf757116c
Hector Agudelo <haacny86@gmail.com>
2022-10-21 17:59:48 -0400

    feat: partial navbar and color palette added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
       secondary:'#3a8461',
       tertiary:'#d7cea6',
       quaternary:' #eff2e7',
-      white: '#ffffff',
-      black: '#00000'
+      white: '#efefef',
+      black: '#0a0a0a'
     },
     extend: {},
   },
