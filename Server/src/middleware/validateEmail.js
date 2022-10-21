33b584464aba5930286458a89bb45276ea71797f
lincoln gibson <lincolngibson7@gmail.com>
2022-10-21 20:30:43 -0300

    edited email validation middleware

--- Server/src/middleware/validateEmail.js
+++ Server/src/middleware/validateEmail.js
@@ -1,7 +1,25 @@
 // TODO update this file with the actual email validator
 
-const validateEmail = (req, res, next) => {
+module.exports = (req, res, next) => {
+	const { email, name, password } = req.body;
+
+	const validateEmail = (userEmail) => {
+		return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+			userEmail
+		);
+	};
+	if (req.path === "/signup") {
+		if (![email, name, password].every(Boolean)) {
+			return res.status(401).json("Missing Credentials");
+		} else if (!validateEmail(email)) {
+			return res.status(401).json("Invalid Email.");
+		}
+	} else if (req.path === "/login ") {
+		if (![email, password].every(Boolean)) {
+			return res.status(401).json("Missing Credentials.");
+		} else if (!validateEmail(email)) {
+			return res.status(401).json("Invalid Email.");
+		}
+	}
 	next();
 };
-
-module.exports = validateEmail;
