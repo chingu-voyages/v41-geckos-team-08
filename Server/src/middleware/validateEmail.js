883f0c4f6c0b65726882b3712d31358fb8596e8c
lincoln gibson <lincolngibson7@gmail.com>
2022-10-22 13:50:13 -0300

    email validator complete

--- Server/src/middleware/validateEmail.js
+++ Server/src/middleware/validateEmail.js
@@ -1,25 +1,12 @@
-// TODO update this file with the actual email validator
+const validateEmail = (req, res, next) => {
+	const { email } = req.body;
+	const mailformat =
+		/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-module.exports = (req, res, next) => {
-	const { email, name, password } = req.body;
-
-	const validateEmail = (userEmail) => {
-		return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
-			userEmail
-		);
-	};
-	if (req.path === "/signup") {
-		if (![email, name, password].every(Boolean)) {
-			return res.status(401).json("Missing Credentials");
-		} else if (!validateEmail(email)) {
-			return res.status(401).json("Invalid Email.");
-		}
-	} else if (req.path === "/login ") {
-		if (![email, password].every(Boolean)) {
-			return res.status(401).json("Missing Credentials.");
-		} else if (!validateEmail(email)) {
-			return res.status(401).json("Invalid Email.");
-		}
+	if (!email.match(mailformat)) {
+		return res.status(401).json("You have entered an Invalid email address!");
 	}
 	next();
 };
+
+module.exports = validateEmail;
