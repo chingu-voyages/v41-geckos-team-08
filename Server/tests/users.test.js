5e39b72249fca05dabfa81994861c0cb7366a99e
Andrés Court <andres@andrescourt.com>
2022-10-23 18:00:51 -0500

    testing: users get one bad uuid

--- Server/tests/users.test.js
+++ Server/tests/users.test.js
@@ -28,25 +28,6 @@ describe('Test the users path', () => {
 		await client.end();
 	});
 
-	describe('Given the supplier query parameter', () => {
-		it('It should return 406 if it not exists', async () => {
-			const response = await request(baseUrl)
-				.get('/users')
-				.expect('Content-Type', /json/);
-
-			expect(response.statusCode).toBe(406);
-			expect(response.body.detail).toBe(
-				'Query parameter supplier is required'
-			);
-		});
-		it('It should return 200 if it exists', async () => {
-			const response = await request(baseUrl)
-				.get('/users?supplier=true')
-				.expect('Content-Type', /json/);
-
-			expect(response.statusCode).toBe(200);
-		});
-	});
 	describe('Test for creating a user', () => {
 		describe('Given the JSON body is incomplete', () => {
 			it('should return 401 when not sending an invalid email', async () => {
@@ -83,6 +64,7 @@ describe('Test the users path', () => {
 				expect(response.statusCode).toBe(400);
 			});
 		});
+
 		describe('Given the JSON body is complete', () => {
 			it('should return 201 when creating a supplier', async () => {
 				response = await request(baseUrl).post('/users').send({
@@ -94,12 +76,19 @@ describe('Test the users path', () => {
 				});
 				expect(response.statusCode).toBe(201);
 				expect(response.body.data).toHaveProperty('uuid');
+				expect(typeof response.body.data.uuid).toBe('string');
 				expect(response.body.data).toHaveProperty('email');
+				expect(typeof response.body.data.email).toBe('string');
 				expect(response.body.data).toHaveProperty('name');
+				expect(typeof response.body.data.name).toBe('string');
 				expect(response.body.data).toHaveProperty('phone');
+				expect(typeof response.body.data.phone).toBe('string');
 				expect(response.body.data).toHaveProperty('is_supplier');
+				expect(typeof response.body.data.is_supplier).toBe('boolean');
 				expect(response.body.data).toHaveProperty('trades');
+				expect(typeof response.body.data.trades).toBe('object');
 				expect(response.body.data).toHaveProperty('cities');
+				expect(typeof response.body.data.cities).toBe('object');
 				expect(response.body.data).not.toHaveProperty('password');
 			});
 			it('should return 201 when creating a non supplier user', async () => {
@@ -112,12 +101,19 @@ describe('Test the users path', () => {
 				});
 				expect(response.statusCode).toBe(201);
 				expect(response.body.data).toHaveProperty('uuid');
+				expect(typeof response.body.data.uuid).toBe('string');
 				expect(response.body.data).toHaveProperty('email');
+				expect(typeof response.body.data.email).toBe('string');
 				expect(response.body.data).toHaveProperty('name');
+				expect(typeof response.body.data.name).toBe('string');
 				expect(response.body.data).toHaveProperty('phone');
+				expect(typeof response.body.data.phone).toBe('string');
 				expect(response.body.data).toHaveProperty('is_supplier');
+				expect(typeof response.body.data.is_supplier).toBe('boolean');
 				expect(response.body.data).toHaveProperty('trades');
+				expect(typeof response.body.data.trades).toBe('object');
 				expect(response.body.data).toHaveProperty('cities');
+				expect(typeof response.body.data.cities).toBe('object');
 				expect(response.body.data).not.toHaveProperty('password');
 			});
 			it('should return 409 when trying to create a user with existing email', async () => {
@@ -143,6 +139,25 @@ describe('Test the users path', () => {
 	});
 
 	describe('Getting all of the users', () => {
+		describe('Given the supplier query parameter', () => {
+			it('It should return 406 if it not exists', async () => {
+				const response = await request(baseUrl)
+					.get('/users')
+					.expect('Content-Type', /json/);
+
+				expect(response.statusCode).toBe(406);
+				expect(response.body.detail).toBe(
+					'Query parameter supplier is required'
+				);
+			});
+			it('It should return 200 if it exists', async () => {
+				const response = await request(baseUrl)
+					.get('/users?supplier=true')
+					.expect('Content-Type', /json/);
+
+				expect(response.statusCode).toBe(200);
+			});
+		});
 		describe('Given the supplier query parameter is false', () => {
 			it('Should return only non suppliers', async () => {
 				const response = await request(baseUrl).get(
@@ -153,6 +168,21 @@ describe('Test the users path', () => {
 
 				data.forEach((user) => {
 					expect(user.is_supplier).toBe(false);
+					expect(user).toHaveProperty('uuid');
+					expect(typeof user.uuid).toBe('string');
+					expect(user).toHaveProperty('email');
+					expect(typeof user.email).toBe('string');
+					expect(user).toHaveProperty('name');
+					expect(typeof user.name).toBe('string');
+					expect(user).toHaveProperty('phone');
+					expect(typeof user.phone).toBe('string');
+					expect(user).toHaveProperty('is_supplier');
+					expect(typeof user.is_supplier).toBe('boolean');
+					expect(user).toHaveProperty('trades');
+					expect(typeof user.trades).toBe('object');
+					expect(user).toHaveProperty('cities');
+					expect(typeof user.cities).toBe('object');
+					expect(user).not.toHaveProperty('password');
 				});
 			});
 		});
@@ -166,7 +196,52 @@ describe('Test the users path', () => {
 
 				data.forEach((user) => {
 					expect(user.is_supplier).toBe(true);
+					expect(user).toHaveProperty('uuid');
+					expect(typeof user.uuid).toBe('string');
+					expect(user).toHaveProperty('email');
+					expect(typeof user.email).toBe('string');
+					expect(user).toHaveProperty('name');
+					expect(typeof user.name).toBe('string');
+					expect(user).toHaveProperty('phone');
+					expect(typeof user.phone).toBe('string');
+					expect(user).toHaveProperty('is_supplier');
+					expect(typeof user.is_supplier).toBe('boolean');
+					expect(user).toHaveProperty('trades');
+					expect(typeof user.trades).toBe('object');
+					expect(user).toHaveProperty('cities');
+					expect(typeof user.cities).toBe('object');
+					expect(user).not.toHaveProperty('password');
 				});
+			});
+		});
+	});
+
+	describe('Getting one user', () => {
+		let userUUID;
+
+		beforeAll(async () => {
+			const response = await request(baseUrl).get('/users?supplier=true');
+			console.log(response.body);
+			userUUID =
+				response.body.data[
+					Math.floor(Math.random() * response.body.data.length)
+				].uuid;
+		});
+		describe('Given the UUID', () => {
+			it('Should return 400 if the UUID is invalid', async () => {
+				const url = `/users/thisanin-vali-duui-dsoi-treturn404nf`;
+				const response = await request(baseUrl).get(url);
+
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 400 with valid UUID inexistent in users', async () => {
+				const badUUID = 'a1bcdef2-1adc-d551-d701-74bacde40433';
+				const url = `/users/${badUUID}`;
+				const response = await request(baseUrl).get(url);
+
+				// console.log(response.body.detail);
+
+				expect(response.statusCode).toBe(404);
 			});
 		});
 	});
