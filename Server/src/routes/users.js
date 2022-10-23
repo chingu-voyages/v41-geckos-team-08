f96ce558aff3598801f800795545a95258441af6
lincoln gibson <lincolngibson7@gmail.com>
2022-10-23 10:51:44 -0300

    fixed logic in user post route.

--- Server/src/routes/users.js
+++ Server/src/routes/users.js
@@ -1,13 +1,13 @@
-const route = require('express').Router();
-const client = require('../config/db');
-const uuid = require('uuid');
-const bcrypt = require('bcrypt');
+const route = require("express").Router();
+const client = require("../config/db");
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
-const { formatOneUserResponse } = require('../components/format');
-const { saveCities } = require('../components/saveCities');
-const { saveTrades } = require('../components/saveTrades');
-const validateUUID = require('../middleware/validateUUID');
-const validateEmail = require('../middleware/validateEmail'); // TODO update this with the actual validate Email middleware
+const { formatOneUserResponse } = require("../components/format");
+const { saveCities } = require("../components/saveCities");
+const { saveTrades } = require("../components/saveTrades");
+const validateUUID = require("../middleware/validateUUID");
+const validateEmail = require("../middleware/validateEmail"); // TODO update this with the actual validate Email middleware
 
 // TODO add authentication middelware once created
 
@@ -16,30 +16,30 @@ const hashPassword = async (plainPassword) => {
 	return await bcrypt.hash(plainPassword, 10);
 };
 
-route.get('/', async (req, res) => {
+route.get("/", async (req, res) => {
 	// Validation that the request have a query parameter named supplier and its value is either true or false
 	if (!req.query.supplier) {
 		res.status(406).json({
-			detail: 'Query parameter supplier is required',
+			detail: "Query parameter supplier is required",
 		});
 		return;
 	}
 
 	if (
-		req.query.supplier.toLowerCase() !== 'true' &&
-		req.query.supplier.toLowerCase() !== 'false'
+		req.query.supplier.toLowerCase() !== "true" &&
+		req.query.supplier.toLowerCase() !== "false"
 	) {
 		res.status(406).json({
-			detail: 'Query parameter supplier value must be true or false',
+			detail: "Query parameter supplier value must be true or false",
 		});
 		return;
 	}
 
-	const is_supplier = req.query.supplier.toLowerCase() === 'true';
+	const is_supplier = req.query.supplier.toLowerCase() === "true";
 	// End of validation
 
 	const sql =
-		'select uuid, email, is_supplier, name, phone from users where is_supplier = $1';
+		"select uuid, email, is_supplier, name, phone from users where is_supplier = $1";
 	const response = await client.query(sql, [is_supplier]);
 	const responseData = [];
 
@@ -50,41 +50,39 @@ route.get('/', async (req, res) => {
 	}
 
 	res.status(200).json({
-		'total-results': response.rowCount,
+		"total-results": response.rowCount,
 		data: responseData,
 	});
 });
 
-route.post('/', validateEmail, async (req, res) => {
+route.post("/", validateEmail, async (req, res) => {
 	const userData = req.body;
 
 	if (!req.body.email)
-		return res.status(400).json({ detail: 'Email is a required property' });
+		return res.status(400).json({ detail: "Email is a required property" });
 	if (!req.body.password)
-		return res
-			.status(400)
-			.json({ detail: 'Password is a required property' });
+		return res.status(400).json({ detail: "Password is a required property" });
 	if (!req.body.name)
 		return res
 			.status(400)
 			.json({ detail: "User's name is a required property" });
 
-	if (!req.body.is_supplier)
+	if (!req.body.is_supplier && null)
 		return res
 			.status(400)
-			.json({ detail: 'Have to specify if user is supplier' });
+			.json({ detail: "Have to specify if user is supplier" });
 
 	if (!req.body.phone)
 		return res
 			.status(400)
 			.json({ detail: "User's phone is a required property" });
 
-	await client.query('BEGIN'); // Start a transaction nothing will be saved until a commit
+	await client.query("BEGIN"); // Start a transaction nothing will be saved until a commit
 
 	try {
 		const userUUID = uuid.v4(); // Generates a new UUID to use in the user
 		let sql =
-			'insert into users (uuid, email, password, is_supplier, is_active, name, phone) values ($1, $2, $3, $4, $5, $6, $7)';
+			"insert into users (uuid, email, password, is_supplier, is_active, name, phone) values ($1, $2, $3, $4, $5, $6, $7)";
 
 		// Hashes the password so we do not save passwords that anyone can read
 		const hashedPassword = await hashPassword(userData.password);
@@ -94,7 +92,7 @@ route.post('/', validateEmail, async (req, res) => {
 			userData.email,
 			hashedPassword,
 			userData.is_supplier,
-			true,
+			true || false,
 			userData.name,
 			userData.phone || null,
 		];
@@ -104,8 +102,8 @@ route.post('/', validateEmail, async (req, res) => {
 		// If the request have an array of cities, then it saves them
 		if (userData.cities && userData.cities.length > 0) {
 			if (!(await saveCities(userData.cities, userUUID))) {
-				await client.query('ROLLBACK');
-				res.status(406).json({ detail: 'Invalid city uuid' });
+				await client.query("ROLLBACK");
+				res.status(406).json({ detail: "Invalid city uuid" });
 				return;
 			}
 		}
@@ -113,18 +111,18 @@ route.post('/', validateEmail, async (req, res) => {
 		// If the request have an array of trades, then it saves them
 		if (userData.trades && userData.trades.length > 0) {
 			if (!(await saveTrades(userData.trades, userUUID))) {
-				await client.query('ROLLBACK');
-				res.status(406).json({ detail: 'Invalid trade uuid' });
+				await client.query("ROLLBACK");
+				res.status(406).json({ detail: "Invalid trade uuid" });
 				return;
 			}
 		}
 
-		await client.query('COMMIT'); // Saves all the information to the database
+		await client.query("COMMIT"); // Saves all the information to the database
 
 		// if saving was succefsull then we get the saved information
 
 		sql =
-			'select uuid, email, is_supplier, name, phone from users where uuid = $1';
+			"select uuid, email, is_supplier, name, phone from users where uuid = $1";
 		const userResponse = await (await client.query(sql, [userUUID])).rows;
 
 		const responseData = await formatOneUserResponse(userResponse[0]);
@@ -134,20 +132,20 @@ route.post('/', validateEmail, async (req, res) => {
 			data: responseData,
 		});
 	} catch (err) {
-		await client.query('ROLLBACK'); // On any error it will rollback
+		await client.query("ROLLBACK"); // On any error it will rollback
 		console.error(err);
 		// Sends to the user the error information with a status code of 409
 		res.status(409).json({
-			detail: 'Conflict',
+			detail: "Conflict",
 		});
 	}
 });
 
-route.get('/:uuid', validateUUID, async (req, res) => {
+route.get("/:uuid", validateUUID, async (req, res) => {
 	const userUUID = req.params.uuid;
 
 	const sql =
-		'select uuid, email, is_supplier, name, phone from users where uuid = $1';
+		"select uuid, email, is_supplier, name, phone from users where uuid = $1";
 	const userResponse = await await client.query(sql, [userUUID]);
 
 	if (userResponse.rowCount === 0) {
@@ -164,12 +162,12 @@ route.get('/:uuid', validateUUID, async (req, res) => {
 	});
 });
 
-route.put('/:uuid', validateUUID, validateEmail, async (req, res) => {
+route.put("/:uuid", validateUUID, validateEmail, async (req, res) => {
 	const userUUID = req.params.uuid;
 	const userData = req.body;
 
 	let sql =
-		'select uuid, email, is_supplier, name, phone from users where uuid = $1';
+		"select uuid, email, is_supplier, name, phone from users where uuid = $1";
 	const userResponse = await client.query(sql, [userUUID]);
 
 	if (userResponse.rowCount === 0) {
@@ -183,36 +181,35 @@ route.put('/:uuid', validateUUID, validateEmail, async (req, res) => {
 	const email = userData.email || userResponse.rows[0].email;
 	const name = userData.name || userResponse.rows[0].name;
 	const phone = userData.phone || userResponse.rows[0].phone;
-	const is_supplier =
-		userData.is_supplier || userResponse.rows[0].is_supplier;
+	const is_supplier = userData.is_supplier || userResponse.rows[0].is_supplier;
 
 	let values = [];
 
 	// Since we can not read the password then if the user doesn't send it then it will not update the password
 	if (userData.password) {
 		sql =
-			'update users set email=$1, password=$2, name=$3, phone=$4, is_supplier=$5 where uuid=$6';
+			"update users set email=$1, password=$2, name=$3, phone=$4, is_supplier=$5 where uuid=$6";
 
 		const hashedPassword = await hashPassword(userData.password);
 
 		values = [email, hashedPassword, name, phone, is_supplier, userUUID];
 	} else {
 		sql =
-			'update users set email=$1, name=$2, phone=$3, is_supplier=$4 where uuid=$5';
+			"update users set email=$1, name=$2, phone=$3, is_supplier=$4 where uuid=$5";
 
 		values = [email, name, phone, is_supplier, userUUID];
 	}
 
 	try {
-		await client.query('BEGIN');
+		await client.query("BEGIN");
 
 		await client.query(sql, values);
 
 		// If the request have an array of cities, then it saves them
 		if (userData.cities && userData.cities.length > 0) {
 			if (!(await saveCities(userData.cities, userUUID))) {
-				await client.query('ROLLBACK');
-				res.status(406).json({ detail: 'Invalid city uuid' });
+				await client.query("ROLLBACK");
+				res.status(406).json({ detail: "Invalid city uuid" });
 				return;
 			}
 		}
@@ -220,18 +217,18 @@ route.put('/:uuid', validateUUID, validateEmail, async (req, res) => {
 		// If the request have an array of trades, then it saves them
 		if (userData.trades && userData.trades.length > 0) {
 			if (!(await saveTrades(userData.trades, userUUID))) {
-				await client.query('ROLLBACK');
-				res.status(406).json({ detail: 'Invalid trade uuid' });
+				await client.query("ROLLBACK");
+				res.status(406).json({ detail: "Invalid trade uuid" });
 				return;
 			}
 		}
 
-		await client.query('COMMIT'); // Saves all the information to the database
+		await client.query("COMMIT"); // Saves all the information to the database
 
 		// if saving was succefsull then we get the saved information
 
 		sql =
-			'select uuid, email, is_supplier, name, phone from users where uuid = $1';
+			"select uuid, email, is_supplier, name, phone from users where uuid = $1";
 		const userResponse = await (await client.query(sql, [userUUID])).rows;
 
 		const responseData = await formatOneUserResponse(userResponse[0]);
@@ -240,8 +237,8 @@ route.put('/:uuid', validateUUID, validateEmail, async (req, res) => {
 			data: responseData,
 		});
 	} catch (err) {
-		await client.query('ROLLBACK');
-		res.status(409).json({ detail: 'Conflict' });
+		await client.query("ROLLBACK");
+		res.status(409).json({ detail: "Conflict" });
 	}
 });
 
