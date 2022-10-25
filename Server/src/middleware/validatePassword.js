9e6cbcffc0c60f7e675a3e75938c39b2b55843a1
lincoln gibson <lincolngibson7@gmail.com>
2022-10-25 10:20:19 -0300

    refactored the code and simplified the logic.

--- Server/src/middleware/validatePassword.js
+++ Server/src/middleware/validatePassword.js
@@ -2,20 +2,14 @@ const validatePassword = (req, res, next) => {
 	const { password } = req.body;
 
 	const regPassword =
-		/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+		/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{7,15}$/;
 
-	if (password && password.length < 6) {
-		return res.status(401).json("Password must be atleast 6 characters long.");
-	}
 	if (!password.match(regPassword)) {
 		return res
-			.status(401)
+			.status(403)
 			.json(
 				"Password must contain atleast one uppercase letter, number and symbol."
 			);
-	}
-	if (password.match(regPassword)) {
-		res.status(200);
 	}
 	next();
 };
