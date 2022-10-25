8406663298ab4896c8c0928b9f8594dc5c383ded
Andrés Court <andres@andrescourt.com>
2022-10-25 18:42:56 -0500

    Feature: Data Validation

--- Server/src/routes/jobs.js
+++ Server/src/routes/jobs.js
@@ -1,9 +1,80 @@
 const route = require('express').Router();
+const client = require('../config/db');
+const { isValidUUID } = require('../middleware/validateUUID');
+const moment = require('moment');
 
-route.get('/', (req, res) => {
+route.get('/', async (req, res) => {
+	if (!req.query.city)
+		return res.status(406).json({ detail: 'You need to send the city' });
+
+	const jobsSQL =
+		'select job.uuid, job.trade_uuid, job.customer_uuid, job.supplier_uuid, job.city_uuid, job.description, job.low_price, job.high_price, job.expiration_date, job.is_taken, job.is_completed from job join city on job.city_uuid = city.uuid where city.name = $1';
+
+	const jobsResponse = await client.query(jobsSQL, [req.query.city]);
+
 	res.json({
-		data: 'Jobs working',
+		'total-result': jobsResponse.rowCount,
+		data: jobsResponse.rows,
 	});
+});
+
+route.post('/', async (req, res) => {
+	const {
+		trade_uuid,
+		customer_uuid,
+		city_uuid,
+		description,
+		low_price,
+		high_price,
+		expiration_date,
+	} = req.body;
+
+	if (!trade_uuid)
+		return res
+			.status(400)
+			.json({ detail: 'The trade is a required property' });
+
+	if (!customer_uuid)
+		return res
+			.status(400)
+			.json({ detail: 'The customer is a required property' });
+
+	if (!city_uuid)
+		return res
+			.status(400)
+			.json({ detail: 'The city is a required property' });
+
+	if (!description)
+		return res
+			.status(400)
+			.json({ detail: 'The description is a required property' });
+
+	if (!isValidUUID(trade_uuid))
+		return res.status(400).json({ detail: 'Invalid trade uuid' });
+
+	if (!isValidUUID(customer_uuid))
+		return res.status(400).json({ detail: 'Invalid trade uuid' });
+
+	if (!isValidUUID(city_uuid))
+		return res.status(400).json({ detail: 'Invalid trade uuid' });
+
+	if (description.length < 10)
+		return res.status(400).json({
+			detail: 'Description must be at least 10 characters long',
+		});
+
+	if (low_price !== undefined && typeof low_price !== 'number')
+		return res.status(422).json({ detail: 'Low price must be a number' });
+
+	if (high_price !== undefined && typeof high_price !== 'number')
+		return res.status(422).json({ detail: 'High price must be a number' });
+
+	if (!moment(expiration_date, 'YYYY-MM-DD', true).isValid())
+		return res.status(422).json({
+			detail: 'Invalid expiration date, it must be in the format YYYY-MM-DD',
+		});
+
+	res.status(200).json({ data: [] });
 });
 
 module.exports = route;
---
+++ Server/tests/routes/jobs.test.js
@@ -0,0 +1,181 @@
+const request = require('supertest');
+// const baseUrl = require('../../test-config');
+const baseUrl = require('../../app');
+const client = require('../../src/config/db');
+const { initializeDB } = require('../common/initializeDB');
+
+const endpoint = '/jobs';
+
+describe('Test jobs', () => {
+	beforeAll(async () => {
+		await initializeDB(client);
+	});
+	afterAll(async () => {
+		await client.end();
+	});
+	describe('Getting all of the jobs', () => {
+		describe('Given the city query parameter', () => {
+			it('should return 406 when it does not exist', async () => {
+				const response = await request(baseUrl).get(endpoint);
+
+				expect(response.statusCode).toBe(406);
+			});
+			it('should return 200 if it exists', async () => {
+				const url = `${endpoint}?city=quito`;
+				console.log(url);
+				const response = await request(baseUrl).get(url);
+
+				expect(response.statusCode).toBe(200);
+				expect(typeof response.body.data).toBe('object');
+
+				const jobs = response.body.data;
+
+				jobs.forEach((job) => {
+					expect(job).toHaveProperty('uuid');
+					expect(job).toHaveProperty('description');
+					expect(job).toHaveProperty('low_price');
+					expect(job).toHaveProperty('high_price');
+					expect(job).toHaveProperty('expiration_date');
+					expect(job).toHaveProperty('is_taken');
+					expect(job).toHaveProperty('is_completed');
+				});
+			});
+		});
+	});
+	describe('Create a job', () => {
+		describe('Given the json is incomplete', () => {
+			it('should return 400 if there is no trade_uuid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 400 if there is no customer_uuid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 400 if there is no city_uuid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should reurn 400 if there is no description', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+		});
+		describe('Given the json is complete', () => {
+			it('should return 400 if trade_uuid is invalid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'thisanin-vali-duui-dsoi-treturn404nf',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					low_price: 0,
+					high_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 400 if customer_uuid is invalid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'thisanin-vali-duui-dsoi-treturn404nf',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					low_price: 0,
+					high_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 400 if city_uuid is invalid', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'thisanin-vali-duui-dsoi-treturn404nf',
+					description: 'asdfghjklñdgdghdghd',
+					low_price: 0,
+					high_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it("should reurn 400 if there is no description doesn't have at least 10 characters", async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfgh',
+					low_price: 0,
+					high_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(400);
+			});
+			it('should return 422 if the low price is not a number', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					low_price: 'ldalñjf',
+					high_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(422);
+			});
+			it('should return 422 if the high price is not a number', async () => {
+				const response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					high_price: 'ldalñjf',
+					low_price: 0,
+					expiration_date: '2022-12-15',
+				});
+				expect(response.statusCode).toBe(422);
+			});
+			it('should return 422 if the user sends an invalid date', async () => {
+				let response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					high_price: '0',
+					low_price: 0,
+					expiration_date: '2022-13-15',
+				});
+				expect(response.statusCode).toBe(422);
+
+				response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					high_price: '0',
+					low_price: 0,
+					expiration_date: '2022-12-50',
+				});
+				expect(response.statusCode).toBe(422);
+				response = await request(baseUrl).post(endpoint).send({
+					trade_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					customer_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					city_uuid: 'a1bcdef2-1adc-d551-d701-74bacde40433',
+					description: 'asdfghjklñdgdghdghd',
+					high_price: '0',
+					low_price: 0,
+					expiration_date: '123456-12-15',
+				});
+				expect(response.statusCode).toBe(422);
+			});
+		});
+	});
+});
