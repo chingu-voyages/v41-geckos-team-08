1042cea7e6a6a963e712b008b2f454966af1144a
Hector Agudelo <haacny86@gmail.com>
2022-10-24 23:16:23 -0400

    feat: working on the profile card

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import {NavBar} from './Components/NavBar'
 import {Footer} from './Components/Footer.jsx'
 import Logo from './Components/Logo';
+import { ProfileCard } from './Components/ProfileCard';
 
 function App() {
   return (
     <div className="App">
-    <Logo />
-    {/* <Footer/> */}
+    {/* <Logo /> */}
+    <Footer/>
+    <ProfileCard/>
       
      
     </div>
--- client/src/Components/Logo.jsx
+++ client/src/Components/Logo.jsx
@@ -4,8 +4,8 @@ const Logo = () => {
   return (
     <div className='h-11 w-23'>
      {/* I'm having trouble showing the inner div classes below: */}
-     <div className='bg-logo absolute top-0 right-0.3573/1 bottom-0.4911/1 left-0.1486/1 scale-x-0.81 skew-y-0.61 -skew-x-0.57 scale-y-0.81 translate-x-0 translate-y-0'></div>
-     <div className='bg-logo absolute top-0 -right-0.3455/1 bottom-0.4911/1 left-0.8514/1 -scale-x-0.81 skew-y-0.61 skew-x-0.57 scale-y-0.81 translate-x-0 translate-y-0'></div>
+     <div className='bg-primary-200 absolute top-0 right-0.3573/1 bottom-0.4911/1 left-0.1486/1 scale-x-0.81 skew-y-0.61 -skew-x-0.57 scale-y-0.81 translate-x-0 translate-y-0'></div>
+     <div className='bg-primary-200 absolute top-0 -right-0.3455/1 bottom-0.4911/1 left-0.8514/1 -scale-x-0.81 skew-y-0.61 skew-x-0.57 scale-y-0.81 translate-x-0 translate-y-0'></div>
     </div>
   );
 }
