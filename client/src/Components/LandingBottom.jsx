e2a0725af3d2944e9d4430b45b8b6fa60e8c7115
Hector Agudelo <haacny86@gmail.com>
2022-10-29 11:03:21 -0400

    feat:adding the landing page bottom part

--- client/src/Components/LandingBottom.jsx
+++ client/src/Components/LandingBottom.jsx
@@ -5,12 +5,14 @@ export const LandingBottom = () => {
     // <div className='container my-24 px-6 mx-auto'>
     <section classNameName='mb-32 text-black'>
       <div className='block bg-quaternary-100'>
+      <div className='flex justify-center items-center lg:justify-start px-12 py-0  md:px-9'>
+            <h2 className='text-xl font-bold mb-4 pr-0 text-center lg:text-left lg:pr-96 lg:mr-40'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+          </div>
         <div className='flex flex-wrap items-center'>
           <div className='py-0 grow-0 shrink-0 basis-auto w-full md:w-6/12 lg:w-6/12 xl:w-6/12'>
-            <div className=' flex flex-col justify-center items-center md:items-start px-6 py-0  md:px-12'>
-              {/* <h2 className='text-5xl font-bold mb-4 text-center md:text-left'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </h2> */}
+            <div className=' flex flex-col justify-center items-center md:items-start px-6 py-0  md:px-9'>
               <h4 className='text-xl font-bold mt-3'>
                 Lorem ipsum dolor sit amet
               </h4>
@@ -32,7 +34,7 @@ export const LandingBottom = () => {
             </div>
           </div>
           <div className='py-0 grow-0 shrink-0 basis-auto w-full md:w-6/12 lg:w-6/12 xl:w-6/12'>
-            <div className=' flex flex-col justify-center items-center md:items-start px-6 py-0  md:px-12'>
+            <div className=' flex flex-col justify-center items-center md:items-start px-6 py-0  md:px-9'>
               <h4 className='text-xl font-bold mt-3'>
                 Lorem ipsum dolor sit amet
               </h4>
