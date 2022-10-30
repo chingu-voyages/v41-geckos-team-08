d06a2617efaf17eaee9cbbc996202fc528f5ec0e
Hector Agudelo <haacny86@gmail.com>
2022-10-30 12:41:22 -0400

    login page created

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -6,31 +6,16 @@ import { Footer } from "../Components/Footer";
 export const LoginPage=()=> {
   return (
     <div className='bg-white  rounded-sm border-none flex flex-col'>
-      <NavBar />
-      <section className='mb-0 text-black'>
-        <div className='block bg-quaternary-100'>
-          <div className='flex flex-wrap items-center'>
-            <div className='py-0 grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-6/12'>
-              <div className=' flex flex-col justify-center items-center px-6 py-12  md:px-12'>
-                <h1 className='text-5xl font-bold mb-4 text-center'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/> Quo
-                  corrupti, saepe 
-                </h1>
-                <LoginForm/>
-              </div>
-            </div>
-            <div className='hidden lg:flex grow-0 shrink-0 basis-90 lg:w-6/12 xl:w-6/12'>
-            <img
-                src='https://mdbootstrap.com/img/new/ecommerce/vertical/088.jpg'
-                alt='Trendy Pants and Shoes'
-                className='w-full'
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-      <Footer />
-    </div>
+    <NavBar />
+
+   
+              <LoginForm />
+    
+
+     
+       
+    <Footer />
+  </div>
   );
 }
   
