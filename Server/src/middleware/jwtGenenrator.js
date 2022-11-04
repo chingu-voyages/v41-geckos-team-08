b8e15df2dc56a52a880efd81467110b770bc4870
lincoln gibson <lincolngibson7@gmail.com>
2022-11-04 10:38:04 -0300

    changed what is provided in the token.

--- Server/src/middleware/jwtGenenrator.js
+++ Server/src/middleware/jwtGenenrator.js
@@ -1,16 +1,17 @@
-const jwt = require('jsonwebtoken');
-const { secret_key } = require('../../environment');
+const jwt = require("jsonwebtoken");
+const { secret_key } = require("../../environment");
 
-const jwtGenerator = (uuid, is_supplier) => {
+const jwtGenerator = (uuid, is_supplier, email) => {
 	const payload = {
 		user: {
 			uuid,
 			is_supplier,
+			email,
 		},
 	};
 	return jwt.sign(payload, secret_key, {
-		algorithm: 'HS256',
-		expiresIn: '1hr',
+		algorithm: "HS256",
+		expiresIn: "1hr",
 	});
 };
 
