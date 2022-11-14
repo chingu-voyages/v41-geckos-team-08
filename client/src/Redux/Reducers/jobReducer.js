a75e3ef02733d894058eaee220e564dd8b12b81b
jonahBerlyne <jonshaber@gmail.com>
2022-11-13 19:25:10 -0500

    added get jobs case to reducer

--- client/src/Redux/Reducers/jobReducer.js
+++ client/src/Redux/Reducers/jobReducer.js
@@ -1,9 +1,11 @@
-import { CREATE_JOB_BY_USER_ID } from '../ActionTypes';
+import { CREATE_JOB_BY_USER_ID, GET_JOBS } from '../ActionTypes';
 
 export const jobReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_JOB_BY_USER_ID:
-      return [...state, action.payload]
+      return [...state, action.payload];
+    case GET_JOBS:
+      return action.payload;
     default:
       return state;
   }
