b667049b9f5d9def0c8405d923d36004770ab6de
jonahBerlyne <jonshaber@gmail.com>
2022-11-16 17:48:21 -0500

    created accepted/rejected proposal fns

--- client/src/Redux/Reducers/jobReducer.js
+++ client/src/Redux/Reducers/jobReducer.js
@@ -1,12 +1,20 @@
-import { CREATE_JOB_BY_USER_ID, GET_JOBS, CREATE_JOB_PROPOSAL } from '../ActionTypes';
+import { 
+  CREATE_JOB_BY_USER_ID, 
+  GET_JOBS, 
+  ACCEPT_JOB_PROPOSAL,
+  REJECT_JOB_PROPOSAL 
+} from '../ActionTypes';
 
 export const jobReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_JOB_BY_USER_ID:
       return [...state, action.payload];
     case GET_JOBS:
-      return action.payload;
-    case CREATE_JOB_PROPOSAL:
+      const jobs_posted = action.payload.filter(job => job.is_taken === false);
+      const jobs_in_progress = action.payload.filter(job => job.is_taken === true);
+      return [{ jobs_posted, jobs_in_progress }];
+    case ACCEPT_JOB_PROPOSAL:
+    case REJECT_JOB_PROPOSAL:
     default:
       return state;
   }
