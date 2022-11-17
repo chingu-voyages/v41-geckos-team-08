f6a0087b02e5e232f206b478a13abb11c97ad35d
Andrés Court <andres@andrescourt.com>
2022-11-16 21:56:12 -0500

    add api to the route and set SERVER constant in client

--- client/src/Utils/Axios.js
+++ client/src/Utils/Axios.js
@@ -1,40 +1,42 @@
 import axios from 'axios';
 
+const SERVER = `http://localhost:8080/api/`;
+
 export const postAPI = async (url, post, token) => {
-  const res = await axios.post(`http://localhost:8080/${url}`, post, {
-    headers: { authorization: token ? token : '' },
-  });
-  return res;
+	const res = await axios.post(`${SERVER}${url}`, post, {
+		headers: { authorization: token ? token : '' },
+	});
+	return res;
 };
 
 export const getAPI = async (url, token) => {
-  const res = await axios.get(`http://localhost:8080/${url}`, {
-    headers: { authorization: token ? token : '' },
-  });
+	const res = await axios.get(`${SERVER}${url}`, {
+		headers: { authorization: token ? token : '' },
+	});
 
-  return res;
+	return res;
 };
 
 export const patchAPI = async (url, post, token) => {
-  const res = await axios.patch(`http://localhost:8080/${url}`, post, {
-    headers: { authorization: token ? token : '' },
-  });
+	const res = await axios.patch(`${SERVER}${url}`, post, {
+		headers: { authorization: token ? token : '' },
+	});
 
-  return res;
+	return res;
 };
 
 export const deleteAPI = async (url, token) => {
-  const res = await axios.delete(`http://localhost:8080/${url}`, {
-    headers: { authorization: token ? token : '' },
-  });
+	const res = await axios.delete(`${SERVER}${url}`, {
+		headers: { authorization: token ? token : '' },
+	});
 
-  return res;
+	return res;
 };
 
 export const putAPI = async (url, post, token) => {
-  const res = await axios.put(`http://localhost:8080/${url}`, post, {
-    headers: { authorization: token ? token : '' },
-  });
+	const res = await axios.put(`${SERVER}${url}`, post, {
+		headers: { authorization: token ? token : '' },
+	});
 
-  return res;
+	return res;
 };
