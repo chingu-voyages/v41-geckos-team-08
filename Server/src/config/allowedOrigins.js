b6464a7e1757711a10354f2faf4c9a3cad0b1a1f
lincoln gibson <lincolngibson7@gmail.com>
2022-12-06 18:16:11 -0300

    updated cors inside appjs and config.

--- Server/src/config/allowedOrigins.js
+++ Server/src/config/allowedOrigins.js
@@ -1,5 +1,6 @@
-const allowedOrigins=[
-'http:localhost:3000/api/'
-'https://v41-geckos-team-08-production.up.railway.app/api/']
+const allowedOrigins = [
+	"http:localhost:3000/api/",
+	"https://v41-geckos-team-08-production.up.railway.app/api/",
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
