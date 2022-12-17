0d3426b3e73cf61738dcd18e5941bc561459abc2
Hector Agudelo <haacny86@gmail.com>
2022-12-17 11:58:20 -0500

    test:adding render tests for the landing page and text

--- client/src/Components/LandingTop.jsx
+++ client/src/Components/LandingTop.jsx
@@ -7,7 +7,7 @@ export const LandingTop = () => {
           <div className='flex flex-wrap items-center'>
             <div className='py-40 grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-6/12'>
               <div className=' flex flex-col justify-center items-center lg:items-start px-6 py-12  md:px-12'>
-                <h1 className='text-5xl font-bold mb-4 text-center lg:text-left leading-snug'>
+                <h1 data-testid='topHeading' className='text-5xl font-bold mb-4 text-center lg:text-left leading-snug'>
                   Need a job done? 
                   <br/>
                   <br/>
