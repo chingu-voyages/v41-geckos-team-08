5df31c4bd17ae341eaa9ee8bfa9acfc0bc71e0cb
jonahBerlyne <jonshaber@gmail.com>
2022-12-21 00:11:08 -0500

    tested no jobs msg on profile page

--- client/src/Pages/UserProfile.jsx
+++ client/src/Pages/UserProfile.jsx
@@ -60,7 +60,7 @@ export const UserProfile = () => {
               />
             </div>
             <div className='flex flex-col justify-center items-center py-12 gap-12 '>
-              <h1 className='text-xl font-bold mb-5 text-center text-black'>
+              <h1 data-testid='jobsHeading' className='text-xl font-bold mb-5 text-center text-black'>
                 {buttonAClicked
                   ? auth.data.is_supplier
                     ? 'Jobs Taken'
@@ -71,16 +71,16 @@ export const UserProfile = () => {
               </h1>
               {buttonAClicked &&
                 !auth.data.is_supplier &&
-                jobs[0].jobs_posted.length === 0 && <h3 className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs posted.</h3>}
+                jobs[0].jobs_posted.length === 0 && <h3 data-testid='noJobs' className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs posted.</h3>}
               {buttonAClicked &&
                 auth.data.is_supplier &&
-                proposals[0].jobs_taken.length === 0 && <h3 className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs picked up.</h3>}
+                proposals[0].jobs_taken.length === 0 && <h3 data-testid='noJobs' className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs picked up.</h3>}
               {buttonBClicked &&
                 !auth.data.is_supplier &&
-                jobs[0].jobs_in_progress.length === 0 && <h3 className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs in progress.</h3>}
+                jobs[0].jobs_in_progress.length === 0 && <h3 data-testid='noJobs' className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs in progress.</h3>}
               {buttonBClicked &&
                 auth.data.is_supplier &&
-                proposals[0].jobs_pending.length === 0 && <h3 className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs waiting to be picked up.</h3>}
+                proposals[0].jobs_pending.length === 0 && <h3 data-testid='noJobs' className='font-bold bg-tertiary-100 rounded p-4 shadow-customShadow text-center w-11/12'>Sorry, you don't have any jobs waiting to be picked up.</h3>}
               {/* I'm gonna have to add logic later differentiating between Jobs Posted and Jobs Picked Up by Suppliers */}
               {buttonAClicked &&
                 !auth.data.is_supplier &&
